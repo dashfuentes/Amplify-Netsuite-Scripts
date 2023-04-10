853e7c48dded73a16297b793244c9292718da89f
Caleb Rodriguez <caleb.rodriguez@rsmus.com>
2023-04-10 16:20:24 -0600

    Tikcet RFS23-652
    - The script now looks for the Item Rate in the FSO Item Line (created from) when the item is not an item group (Kit). The script won't process those items if the SFO doesn’t have a value in the Item Rate Line Column (for not grouped items).
    - The script has been split into a User Event that executes a Map Reduce, which means that is necessary to consider the time taken by the Map Reduce Script to update the Revenue Event Column in the Item Fulfillment Line, the column won’t be updated immediately after the  IF is created/updated, will be updated after the MR Script finishes.

---
+++ RFS23-654/RSM_MR_Revenue_IF_Recognition.js
@@ -0,0 +1,214 @@
+/**
+ * @description
+ * @NApiVersion 2.x
+ * @NScriptType MapReduceScript
+ */
+define(['N/file', 'N/log', 'N/record', 'N/query', 'N/runtime', '../lodash', 'N/search', '../moment'],function(file, log, record, query, runtime, _, search, moment) {
+  /**
+   * @description Marks the beginning of the Map/Reduce process and generates input data.
+   * @typedef {Object} ObjectRef
+   * @property {number} id - Internal ID of the record instance
+   * @property {string} type - Record type id
+   * @return {Array|Object|Search|RecordRef} inputSummary
+   */
+  function getInputData() {
+    var transactionId = runtime.getCurrentScript().getParameter({ name: 'custscript_rsm_mr_transactionid'});
+    log.debug('transaction id from MR', transactionId)
+    if(!transactionId) {
+      throw new Error("The Script parameter custscript_rsm_mr_transactionid was not provided!")
+    }
+
+    // Condition: Item Fulfillment created off of Transfer Order should be excluded
+    // Filter: FSO.type != 'TrnfrOrd'
+    //
+    // Remove: AND DSO.tranid LIKE 'DSO%', for testing only
+    //
+    return query
+      .runSuiteQL({
+        query: "SELECT DISTINCT \
+          IFF.id, \
+          IFF.trandate, \
+          FSO.type AS created_from_type, \
+          IFIT.item, \
+          IFIT.custcol_rsm_product_id AS product_id, \
+          DSOIT.uniquekey AS lineuniquekey, \
+          IFIT.quantity, \
+          IFIT.itemtype, \
+          IFIT.kitmemberof, \
+          COALESCE(IFIT.custcol_rsm_component_rate, 0) AS component_rate, \
+          COALESCE(FSOIT.custcol_rfs23_cwgp_so_fso_item_rate, 0) AS item_rate, \
+          IFIT.custcol_rev_event_rec AS revenue_event_id \
+        FROM Transaction AS IFF \
+        INNER JOIN TransactionLine IFIT ON (IFF.id = IFIT.transaction) \
+        INNER JOIN TransactionLine AS DSOIT ON (DSOIT.custcol_rsm_product_id = IFIT.custcol_rsm_product_id) \
+        INNER JOIN Transaction AS DSO ON (DSO.id = DSOIT.transaction) \
+        INNER JOIN PreviousTransactionLineLink AS PTLL ON (PTLL.nextdoc = IFF.id) \
+        INNER JOIN Transaction AS FSO ON (FSO.id = PTLL.previousdoc) \
+        INNER JOIN TransactionLine AS FSOIT ON (FSO.id = FSOIT.transaction AND IFIT.item = FSOIT.item) \
+        WHERE IFF.type = 'ItemShip' \
+          AND FSO.type != 'TrnfrOrd' \
+          AND IFIT.custcol_rsm_product_id IS NOT NULL \
+          AND IFIT.custcol_rev_event_rec IS NULL \
+          AND DSO.type = 'SalesOrd' \
+          AND DSO.custbody_rsm_so_type = 1 \
+          AND DSO.tranid LIKE 'DSO%' \
+          AND FSOIT.custcol_cwgp_iff_link IS NOT NULL \
+          AND IFF.id = ?",
+        params: [transactionId]
+      })
+      .asMappedResults();
+  }
+
+  /**
+   * @description Executes when the map entry point is triggered and applies to each key/value pair.
+   * @param {MapSummary} context - Data collection containing the key/value pairs to process through the map stage
+   */
+  function mapStage(context) {
+    log.debug('MAP input', context.value);
+
+    try {
+      var input = JSON.parse(context.value);
+      log.debug('transaction from map', input);
+
+      // Kit parents won't be processed
+      if( input.itemtype !== 'InvtPart' ) return;
+
+      var loadIFRecord = record.load({
+        type: "itemfulfillment",
+        id: input.id
+      });
+      log.debug("record", JSON.stringify(loadIFRecord));
+
+      var getCurrentStatus = loadIFRecord.getText("shipstatus");
+      log.debug("status", getCurrentStatus);
+
+      if (getCurrentStatus !== "Shipped") {
+        log.debug('IFF Status', 'The IFF with status other than Shipped is skipped from the revenue recognition process');
+        return;
+      }
+
+      // Looking for line index
+      var linesCount = loadIFRecord.getLineCount("item");
+      log.debug("count item", linesCount);
+
+      for (var index = 0; index < linesCount; index++) {
+        var itemID = loadIFRecord.getSublistValue({
+          sublistId: "item",
+          fieldId: "item",
+          line: index,
+        });
+
+        if(input.item == itemID) {
+          input.line = index;
+        }
+      }
+      log.debug("item Line", input);
+
+      // Creating Revnue Event
+      var revenueId =  createRevRecognition(input);
+      log.debug('revenue id', revenueId);
+
+      context.write({key: input.id, value: JSON.stringify({ sublistId: "item", line: input.line, fieldId: 'custcol_rev_event_rec', value: revenueId })});
+    } catch(e) {
+      log.error('Map Reduce Script error', e);
+    }
+
+    return 'map complete';
+  }
+
+  /**
+   * @description Executes when the reduce entry point is triggered and applies to each group.
+   * @param {ReduceSummary} context - Data collection containing the groups to process through the reduce stage
+   */
+  function reduce(context) {
+      var itemFulfillment = record.load({ type: "itemfulfillment", id: context.key });
+
+      // Updating all IFF lines with the Recognition Revenue ID
+      _.forEach(context.values, function(val) {
+        var it = JSON.parse(val);
+        itemFulfillment.setSublistValue(it);
+      });
+      itemFulfillment.save();
+  }
+
+  /**
+   * @description Executes when the summarize entry point is triggered and applies to the result set.
+   * @param {Summary} summary - Holds statistics regarding the execution of a map/reduce script
+   */
+  function summarize(summary) {
+    // summarize totals
+    log.audit('summary', summary);
+    log.audit('input stage summary', summary.inputSummary);
+    log.audit('map stage summary', summary.mapSummary);
+    log.audit('reduce stage summary', summary.reduceSummary);
+    _.forEach(summary.output.iterator(), function(k, v) {
+      log.audit('summary.output key,value', k + ', ' + v);
+      return true;
+    });
+    log.error('errors', JSON.stringify(summary.mapSummary.errors));
+  }
+
+  function createRevRecognition(data) {
+    try {
+      var newRecogntionRecord = record.create({
+        type: "billingrevenueevent",
+        isDynamic: true,
+      });
+
+      //Unique transaction item line
+      newRecogntionRecord.setValue({
+        fieldId: "transactionline",
+        value: parseInt(data.lineuniquekey),
+      });
+
+      //Event Type
+      newRecogntionRecord.setValue({
+        fieldId: "eventtype",
+        value: 3,
+      });
+
+      var quantity = Math.abs(+data.quantity);
+      //Quantity
+      newRecogntionRecord.setValue({
+        fieldId: "quantity",
+        value: quantity,
+      });
+
+      //Event Purpose
+      newRecogntionRecord.setValue({
+        fieldId: "eventpurpose",
+        value: "ACTUAL",
+      });
+
+      //Event Date
+      var trandate = moment(data.trandate);
+      newRecogntionRecord.setValue({
+        fieldId: "eventdate",
+        value: trandate.toDate(),
+      });
+
+      var amount = (data.kitmemberof ? +data.component_rate : +data.item_rate) * quantity;
+      log.debug('Amount', amount);
+      newRecogntionRecord.setValue({
+        fieldId: "amount",
+        value: amount
+      });
+
+      var revRecord = newRecogntionRecord.save();
+      return revRecord;
+    } catch (error) {
+     return log.debug('Something went wrong!', error);
+    }
+  }
+
+  return {
+    config:{
+      retryCount: 3,
+      exitOnError: false
+    },
+    getInputData: getInputData,
+    map: mapStage,
+    reduce: reduce,
+    summarize: summarize
+  };
+});
