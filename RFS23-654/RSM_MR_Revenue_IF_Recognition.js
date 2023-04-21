e8fe841f7020a3b7e2142433fb2a7f1609b897f5
Caleb Rodriguez <caleb.rodriguez@rsmus.com>
2023-04-21 15:07:32 -0600

    Fixing amount calculation form item groups, removing search filters that are not necessary

--- RFS23-654/RSM_MR_Revenue_IF_Recognition.js
+++ RFS23-654/RSM_MR_Revenue_IF_Recognition.js
@@ -12,11 +12,6 @@ define(['N/file', 'N/log', 'N/record', 'N/query', 'N/runtime', '../lodash', 'N/s
    * @return {Array|Object|Search|RecordRef} inputSummary
    */
   function getInputData() {
-    // Condition: Item Fulfillment created off of Transfer Order should be excluded
-    // Filter: FSO.type != 'TrnfrOrd'
-    //
-    // Remove: AND DSO.tranid LIKE 'DSO%', for testing only
-    //
     return query
       .runSuiteQL({
         query: "SELECT DISTINCT \
@@ -45,9 +40,7 @@ define(['N/file', 'N/log', 'N/record', 'N/query', 'N/runtime', '../lodash', 'N/s
           AND IFIT.custcol_rsm_product_id IS NOT NULL \
           AND IFIT.custcol_rev_event_rec IS NULL \
           AND DSO.type = 'SalesOrd' \
-          AND DSO.custbody_rsm_so_type = 1 \
-          AND DSO.tranid LIKE 'DSO%' \
-          AND FSOIT.custcol_cwgp_iff_link IS NOT NULL",
+          AND DSO.custbody_rsm_so_type = 1",
         params: []
       })
       .asMappedResults();
@@ -184,7 +177,7 @@ define(['N/file', 'N/log', 'N/record', 'N/query', 'N/runtime', '../lodash', 'N/s
       value: trandate.toDate(),
     });
 
-    var amount = (data.kitmemberof ? +data.component_rate : +data.item_rate) * quantity;
+    var amount = (+data.component_rate || +data.item_rate) * quantity;
     log.debug('Amount', amount);
     newRecogntionRecord.setValue({
       fieldId: "amount",
