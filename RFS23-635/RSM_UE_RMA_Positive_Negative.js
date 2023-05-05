58b047790c3caa66debc29384e4a8ecdd5a2f58a
dashfuentes <smis117112@gmail.com>
2023-05-05 09:00:07 -0600

    feat: Update BSO fields

--- RFS23-635/RSM_UE_RMA_Positive_Negative.js
+++ RFS23-635/RSM_UE_RMA_Positive_Negative.js
@@ -1,7 +1,7 @@
 /**
  * @NApiVersion 2.x
  * @NScriptType usereventscript
- * Create positive/negative revenue events for Deal Return Authorization
+ * Check necessary fields for M/R functionality 
  */
 define([
   "N/search",
@@ -15,15 +15,48 @@ define([
   function beforeSubmit(context) {
     if (
       context.type === context.UserEventType.CREATE
-      //  ||
-      //DELETE EDIT LATER
-      //  context.type === context.UserEventType.EDIT  //Double check with amplify if this needs ti run on EDIT as well
     ) {
       var newRecord = context.newRecord;
       newRecord.setValue({
         fieldId: "custbody_rsm_process_revenue_event",
         value: true,
       });
+
+      return;
+    }
+
+    //Special when the customer get back to the transaction and checked the Re-Ship? field
+    if(context.type === context.UserEventType.EDIT){
+      log.debug(context)
+      var newRecord = context.newRecord;
+      var reship = newRecord.getValue('custbody_rsm_reship');
+      var reshipProcess = newRecord.getValue('custbody_rsm_reship_process')
+      log.debug(reshipProcess)
+      if(reship && !reshipProcess){
+        //Call Map/Reduce Script
+
+        try {
+          var t = task.create({
+            taskType: task.TaskType.MAP_REDUCE,
+            scriptId: 'customscript_rsm_mr_fram_bso',
+            //deploymentId: '',
+            params: {
+              custscript_rsm_frm_transactionid	: newRecord.id
+          }
+           });
+        t.submit();
+        log.debug('M/R Status', t)
+        } catch (error) {
+          log.error("Something went wrong", error)
+        }
+       
+      //To make sure it was completed by this process
+        newRecord.setValue({
+          fieldId: "custbody_rsm_reship_process",
+          value: true,
+        });
+      }
+     
 
       return;
     }
